8f032c361e374fe347ea73d2ce94ed929f85aa40
Anish Sabale <officialanishsabale@gmail.com>
2025-07-24 23:45:51 +0530

    project commit

---
+++ employee-management-frontend/src/component/FooterComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className='footer'>
+    <span>All Rights Reserved to Anish @2025 &copy; {new Date().getFullYear()}</span>
+</footer>
+
+        </div>
+    )
+}
+
+export default FooterComponent
